c5a026cb04e1bdb7aac0614a0a92af426236c7ca
Ciprian Dorin, Craciun <ciprian@volution.ro>
2009-05-08 15:09:55 +0300

    --

--- tools/make-distribution.bash
+++ tools/make-distribution.bash
@@ -5,9 +5,14 @@ set -e -u -o pipefail
 test "${#}" -eq 0
 
 tar -c ./core --exclude=.gitignore >./dist/jbs-core.tar
-
 tar -c ./users --exclude=.gitignore >./dist/jbs-users.tar
-
 tar -c ./commands --exclude=.gitignore >./dist/jbs-commands.tar
+
+cp ./tools/install-local.bash ./dist
+cp ./tools/install-remote.bash ./dist
+
+tar -C ./dist -c . --exclude=jbs-dist.tar -v >./dist/jbs-dist.tar
+
+gpg --detach-sign --armor ./dist/jbs-dist.tar
 
 exit 0
