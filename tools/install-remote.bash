c865c743edf94fb14016eb24e0516d2e75a490c6
Ciprian Dorin, Craciun <ciprian@volution.ro>
2009-05-08 14:09:39 +0300

    --

--- tools/install-remote.bash
+++ tools/install-remote.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash --verbose
 
 set -e -u -o pipefail
 
--- tools/make-tar.bash
+++ tools/make-tar.bash
@@ -1,9 +1,13 @@
-#!/bin/bash
+#!/bin/bash --verbose
 
 set -e -u -o pipefail
 
 test "${#}" -eq 0
 
-git archive --format=tar --prefix=.jbs/  HEAD >./jbs.tar
+git archive --format=tar HEAD core >./dist/jbs-core.tar
+
+tar -c ./users --exclude=.gitignore >./dist/jbs-users.tar
+
+tar -c ./commands --exclude=.gitignore >./dist/jbs-commands.tar
 
 exit 0
