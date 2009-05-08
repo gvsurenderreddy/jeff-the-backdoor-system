899e706fa92596d397efa5ea51237a07b0f89586
Ciprian Dorin, Craciun <ciprian@volution.ro>
2009-05-08 15:21:48 +0300

    Fixed distribution script.

--- tools/make-distribution.bash
+++ tools/make-distribution.bash
@@ -8,10 +8,13 @@ tar -c ./core --exclude=.gitignore >./dist/jbs-core.tar
 tar -c ./users --exclude=.gitignore >./dist/jbs-users.tar
 tar -c ./commands --exclude=.gitignore >./dist/jbs-commands.tar
 
-cp ./tools/install-local.bash ./dist
-cp ./tools/install-remote.bash ./dist
-
-tar -C ./dist -c . --exclude=jbs-dist.tar -v >./dist/jbs-dist.tar
+tar -c -v \
+		./dist/jbs-core.tar \
+		./dist/jbs-users.tar \
+		./dist/jbs-commands.tar \
+		./tools/install-local.bash \
+		./tools/install-remote.bash \
+	>./dist/jbs-dist.tar
 
 gpg --detach-sign --armor ./dist/jbs-dist.tar
 
