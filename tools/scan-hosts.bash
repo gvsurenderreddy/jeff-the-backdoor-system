120ffa4abd7f00484a331cb4d71fdb92d9435384
Ciprian Dorin, Craciun <ciprian@volution.ro>
2009-07-07 14:57:24 +0300

    --

--- tools/scan-hosts.bash
+++ tools/scan-hosts.bash
@@ -4,7 +4,7 @@ set -e -u -o pipefail || exit 1
 
 test "${#}" -eq 1
 
-sudo nmap -sS -n "${1}" -oG /dev/stdout --host-timeout 30s --max-retries 16 \
+sudo nmap -sP -n "${1}" -oG /dev/stdout --host-timeout 30s --max-retries 16 \
 | grep -E -e 'Status: Up' \
 | grep -o -E -e '[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+'
 
